2acda8830ccd520c267698e0eeefadb3a4b11d52
Kunal Londhe <kunal.shahaji.londhe@gmail.com>
2025-01-12 09:19:23 +0530

    add production-db config

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { Sequelize } from 'sequelize';
 
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: 'postgres',
-  logging: false
+  logging: false,
+
+  // Add SSL configuration for production (Render)
+  ...(isProduction && {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  })
 });
 
 export default sequelize;
